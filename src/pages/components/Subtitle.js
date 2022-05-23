48037091ddeec914d6e54fa027371e12946690cd
geramolinest <geramolest@gmail.com>
2022-05-22 21:09:27 -0600

    Fixing image background

--- src/pages/components/Subtitle.js
+++ src/pages/components/Subtitle.js
@@ -2,6 +2,8 @@ const Subtitle = () =>{
     return(
         <h3 className="subtitle"> 
             I'm a Software Developer<br/>
+            &
+            <br/>
             Data Analyst.
         </h3>
     )
